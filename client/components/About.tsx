6703f269e8701f1f0b43c44d6d0c4c14501e7329
Eilya <iliya.amin2016@gmail.com>
2021-11-26 13:54:51 +0300

    comment section implemented

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -2,6 +2,7 @@ import Layout from "@/components/Layout"
 import type { NextPage } from 'next'
 import Label from "@/subComponents/Label";
 import StickyBar from "@/subComponents/StickyBar";
+import Header from "@/subComponents/Header"
 
 interface Props {
 }
@@ -10,7 +11,10 @@ interface Props {
 const About: NextPage<Props> = ({ children }) => {
     return (
         <Layout>
-            <div className="about pad-default">
+            <div className="pad-default">
+          <Header span="EILYA's Thoughts, stories and ideas." header="About" />
+        </div>
+            <div className="about pad-default-horizontal">
                 <StickyBar />
                 <div className="about-body">
                     <Label labels={["About", "resume"]} />
