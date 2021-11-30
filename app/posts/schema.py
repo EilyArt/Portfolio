a15feaf0c26fcd6980fa25a522baebdb0092206d
EILYA AMIN <eilya@EILYAs-MacBook-Pro.local>
2021-11-30 23:20:18 +0300

    cleaning in posts app

--- app/posts/schema.py
+++ app/posts/schema.py
@@ -28,40 +28,40 @@ class CommentType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    allPosts = graphene.List(PostType)
-    post = graphene.Field(PostType, slug=graphene.String())
-    lastNPosts = graphene.List(PostType, N=graphene.Int())
-    allTaggedPosts = graphene.List(PostType, tag=graphene.String())
-    allTags = graphene.List(TagType)
-    tag = graphene.Field(TagType, name=graphene.String())
-    allComments = graphene.List(CommentType, slug=graphene.String())
 
     # ANCHOR -  GET ALL POSTS
+    allPosts = graphene.List(PostType)
     def resolve_allPosts(self, info, **kwargs):
         return Post.objects.all()
 
     # ANCHOR -  GET POST BY SLUG
+    post = graphene.Field(PostType, slug=graphene.String())
     def resolve_post(self, info, slug):
         return Post.objects.get(slug=slug)
 
     # ANCHOR -  GET POST BY SLUG
+    lastNPosts = graphene.List(PostType, N=graphene.Int())
     def resolve_lastNPosts(self, info, N):
         return Post.objects.order_by('-id')[:N]
 
     # ANCHOR -  GET ALL POSTS OF A TAG
+    allTaggedPosts = graphene.List(PostType, tag=graphene.String())
     def resolve_allTaggedPosts(self, info, tag):
         tagObject = Tag.objects.get(name=tag)
         return Post.objects.all().filter(tags__in=[tagObject.id])
 
     # ANCHOR -  GET ALL TAGS
+    allTags = graphene.List(TagType)
     def resolve_allTags(self, info, **kwargs):
         return Tag.objects.all()
 
     # ANCHOR -  GET POST BY SLUG
+    tag = graphene.Field(TagType, name=graphene.String())
     def resolve_tag(self, info, slug):
         return Post.objects.get(slug=slug)
 
     # ANCHOR -  GET ALL POSTS
+    allComments = graphene.List(CommentType, slug=graphene.String())
     def resolve_allComments(self, info, slug):
         post = Post.objects.get(slug=slug)
         return Comment.objects.all().filter(parent__isnull=True, post=post.id)
