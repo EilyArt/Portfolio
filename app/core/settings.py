6b418a9653dbcafe362e336103e87347fa9b2f9a
EILYA <iliya.amin2016@gmail.com>
2022-03-19 20:40:37 +0300

    fixing actions errors

--- app/core/settings.py
+++ app/core/settings.py
@@ -27,7 +27,10 @@ DEBUG=int(os.environ.get("DEBUG", default=0))
 
 # 'ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS=os.environ.get("ALLOWED_HOSTS").split(' ')
+if type(os.environ.get("ALLOWED_HOSTS")) == type(None):
+    ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+else:
+    ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(' ')
 
 
 # Application definition
