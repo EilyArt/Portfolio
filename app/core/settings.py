1f7f602cfb6e5c733ab75bf65e3d1a6977e2433e
EILYA <iliya.amin2016@gmail.com>
2022-03-19 20:01:34 +0300

    fixing actions errors

--- app/core/settings.py
+++ app/core/settings.py
@@ -25,10 +25,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
-# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(' ')
+# 'LLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'LLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 
+if type(os.environ.get("ALLOWED_HOSTS")) == type(None):
+    ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+else:
+    ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(' ')
 
 # Application definition
 
