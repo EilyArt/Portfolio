4c6f9e7247a52b1c3da45f244aa892b88ad3f8f4
EILYA <iliya.amin2016@gmail.com>
2022-03-28 19:53:30 +0300

    gunicorn added and enabled

---
+++ app/conf/gunicorn_config.py
@@ -0,0 +1,5 @@
+command = '/home/ubuntu/env/bin/gunicorn'
+pythonpath = '/home/ubuntu/core'
+bind = '0.0.0.0:8000' 
+workers = 5
+threads=2
--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -11,13 +11,7 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py flush --no-input
-python manage.py migrate
-python manage.py seed clients --number=1
-python manage.py seed pages --number=1
-python manage.py seed about --number=1
-python manage.py seed projects --number=1
-python manage.py seed posts --number=1
+
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('root', '', '0')" | python manage.py shell
 
 exec "$@"
